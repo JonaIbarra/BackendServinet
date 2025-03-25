dfaaef124efa4a1673191bb020f0e8a9c6d2502d
Jonathan Enrique Ibarra Canales <161864597+JonaIbarra@users.noreply.github.com>
2025-03-25 09:08:08 -0600

    Update db.py

--- config/db.py
+++ config/db.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Configuración de la conexión a la base de datos
-DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_EKDs0CiGYRqSrfjYO6j@backendservinet-animateibarra97-5f64.g.aivencloud.com:11529/defaultdb
+DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_g-tqDl-Sn2nan3BXlnj@mysql-22a90db5-e19063584-e1b0.g.aivencloud.com:22520/defaultdb?ssl_mode=REQUIRED"
+
 
 # Crear el motor de la base de datos
 engine = create_engine(DATABASE_URL)
